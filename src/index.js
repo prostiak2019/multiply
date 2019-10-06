965ca980c8276098043b6bbe3cc3065fcdd80175
prostiak2019 <yarovenkodmv@gmail.com>
2019-10-07 01:29:29 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+  return String (BigInt(first)*BigInt(second));
+  //  здесь надо исползовать BigInt т.к. в тестах используются большие числа
+  //  String преобразует из числа строку.
 }
+
